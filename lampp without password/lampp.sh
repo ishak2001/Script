20602a25da602383a821317cd8a3a7a1bfd01193
Ishak Ugurlu <73301845+ishak2001@users.noreply.github.com>
2023-08-13 20:31:42 +0200

    Update lampp.sh

--- lampp without password/lampp.sh
+++ lampp without password/lampp.sh
@@ -1,30 +1,28 @@
-#!/bin/bash
-
 # System zu Aktualisieren
-sudo apt update -y
-sudo apt upgrade -y
+apt update -y
+apt upgrade -y
 #
 # Benötigte Pakete für die Installation
-sudo apt install ca-certificates apt-transport-https lsb-release gnupg curl nano unzip -y
+apt install ca-certificates apt-transport-https lsb-release gnupg curl nano unzip -y
 #
 # Paket Quelle für PHP
-sudo curl -fsSL https://packages.sury.org/php/apt.gpg -o /usr/share/keyrings/php-archive-keyring.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/php-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+curl -fsSL https://packages.sury.org/php/apt.gpg -o /usr/share/keyrings/php-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/php-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
 #
 #Pakete Aktualisieren
-sudo apt update -y
+apt update -y
 #
 #Web-Server Installieren
-sudo apt install apache2 -y
+apt install apache2 -y
 #
 #PHP und die dazugehörige Module Installieren
-sudo apt install php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-xsl php8.2-zip php8.2-bz2 libapache2-mod-php8.2 -y
+apt install php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-xsl php8.2-zip php8.2-bz2 libapache2-mod-php8.2 -y
 #
 #MariaDB Server, sowie Client Installieren
-sudo apt install mariadb-server mariadb-client -y
+apt install mariadb-server mariadb-client -y
 #
 # Automate mysql_secure_installation
-sudo mysql <<EOF
+mysql <<EOF
 CREATE USER 'admin'@'%' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
 FLUSH PRIVILEGES;
@@ -34,20 +32,20 @@ EOF
 cd /usr/share
 #
 #phpMyAdmin Installieren
-sudo wget --no-check-certificate  https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
+wget --no-check-certificate  https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip -O phpmyadmin.zip
 #
 #
 #phpMyAdmin entpacken
-sudo unzip phpmyadmin.zip
+unzip phpmyadmin.zip
 #
 #Zip Datei Löschen
-sudo rm phpmyadmin.zip
+rm phpmyadmin.zip
 #
 #Ordner namen zu phpmyadmin umbennen
-sudo mv phpMyAdmin-*-all-languages phpmyadmin
+mv phpMyAdmin-*-all-languages phpmyadmin
 #
 #Berechtigung für phpMyAdmin erteilen
-sudo chmod -R 0755 phpmyadmin
+chmod -R 0755 phpmyadmin
 #
 # Konfigurationsdatei Hinzufügen
 echo "<?php
@@ -67,7 +65,7 @@ declare(strict_types=1);
 \$cfg['UploadDir'] = '';
 \$cfg['SaveDir'] = '';
 
-?>" | sudo tee -a /usr/share/phpmyadmin/config.inc.php
+?>" | tee -a /usr/share/phpmyadmin/config.inc.php
 #
 #
 #Konfigurationsdatei Hinzufügen
@@ -90,13 +88,13 @@ echo "Alias /phpmyadmin /usr/share/phpmyadmin
 </Directory>" >> /etc/apache2/conf-available/phpmyadmin.conf
 #
 #phpMyAdmin Aktivieren
-sudo a2enconf phpmyadmin
+a2enconf phpmyadmin
 #
 #Webserver reloaden
-sudo systemctl reload apache2
+systemctl reload apache2
 #
 #Temporär Verzeichnis für phpMyAdmin erstellen
-sudo mkdir /usr/share/phpmyadmin/tmp/
+mkdir /usr/share/phpmyadmin/tmp/
 #
 #Web-Server Rechte erteilen für das Temporär Verzeichnis
-sudo chown -R www-data:www-data /usr/share/phpmyadmin/tmp/
+chown -R www-data:www-data /usr/share/phpmyadmin/tmp/
